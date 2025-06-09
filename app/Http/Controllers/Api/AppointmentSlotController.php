9003488605d4626d6e4f5159fc6fbaff79b75a1c
Farah-Mari <s12326201@stu.najah.edu>
2025-06-10 00:47:06 +0300

    تعديل بسيط

--- app/Http/Controllers/Api/AppointmentSlotController.php
+++ app/Http/Controllers/Api/AppointmentSlotController.php
@@ -19,7 +19,7 @@ class AppointmentSlotController extends Controller
      *     description="A list of slots",
      *     @OA\JsonContent(
      *       type="array",
-     *       @OA\Items(ref="../../../Swagger/Schemas/AppointmentSlot.php")
+     *       @OA\Items(ref="#/components/schemas/AppointmentSlot")
      *     )
      *   )
      * )
@@ -45,7 +45,7 @@ class AppointmentSlotController extends Controller
      *     )
      *   ),
      *   @OA\Response(response=201, description="Created",
-     *     @OA\JsonContent(ref="../../../Swagger/Schemas")
+     *     @OA\JsonContent(ref="#/components/schemas/AppointmentSlot")
      *   )
      * )
      */
@@ -72,7 +72,7 @@ class AppointmentSlotController extends Controller
      *     @OA\Schema(type="string", format="uuid")
      *   ),
      *   @OA\Response(response=200, description="Slot details",
-     *     @OA\JsonContent(ref="../../../Swagger/Schemas")
+     *     @OA\JsonContent(ref="#/components/schemas/AppointmentSlot")
      *   ),
      *   @OA\Response(response=404, description="Not Found")
      * )
@@ -97,7 +97,7 @@ class AppointmentSlotController extends Controller
      *     )
      *   ),
      *   @OA\Response(response=200, description="Updated",
-     *     @OA\JsonContent(ref="../../../Swagger/Schemas")
+     *     @OA\JsonContent(ref="#/components/schemas/AppointmentSlot")
      *   )
      * )
      */
